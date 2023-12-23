60af7c4d30231150be86b5a774c829ea13fa48f8
Hamed Babaei <dev.hamedbabaei@gmail.com>
2023-12-23 22:44:35 +0330

    added cart template

---
+++ src/Components/Cart/navbar.jsx
@@ -0,0 +1,22 @@
+import { RiShoppingCartLine } from "react-icons/ri";
+import React from "react";
+import { useCartContext } from "../context";
+
+const Navbar = () => {
+  const {cart} = useCartContext();
+  return (
+    <header className="navbar navbar-dark bg-dark shadow-sm">
+      <div className="container">
+        <a className="navbar-tool ms-3">
+          <div className="navbar-tool-icon-box bg-secondary">
+            <span className="navbar-tool-label">{cart.length}</span>
+            <RiShoppingCartLine className="navbar-tool-icon" />
+          </div>
+        </a>
+        <p className="h3 mb-0 text-light">useContext & useReducer</p>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
